3b96f153473df0b06d8931456d0e2e362a230992
PaulaHG36 <paula_hanxi_garcia_m-tsapp1ma2324@cev.com>
2025-04-03 13:10:46 +0200

    espacios entre las celdas

--- lib/main.dart
+++ lib/main.dart
@@ -74,14 +74,6 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _dateController37 = TextEditingController();
 
 
-  /*void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }*/
-
-
   @override
   Widget build(BuildContext context) {
     
@@ -97,19 +89,12 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: SingleChildScrollView(
         child: Column(
-         // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            //const Text('You have pushed the button this many times:'),
-           /* Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),*/
-//            const SizedBox(height: 20),
+            const SizedBox(width: 15,height: 30,),
             Row(
-              //mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text('Fecha:'),
-                const SizedBox(width: 10),
+                const SizedBox(width: 10,),
                 SizedBox(
                   width: 200,
                   child: TextField(
@@ -122,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-            const SizedBox(width: 15),
+            const SizedBox(width: 15,),
             Row(
               //mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -140,6 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
+            const SizedBox(width: 15, height: 30,),
             Row(
               children: [
                 Flexible(child: Table(
